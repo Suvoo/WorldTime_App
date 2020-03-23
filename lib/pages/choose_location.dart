db36a2983a857887ec0e6127cf74cda199be6631
Suvoo <suvodeepsinha1981@gmail.com>
2020-03-23 13:05:15 +0530

    Initial commit

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:Colors.grey[200],
+      appBar: AppBar(),
+      body: Text('ch,
+    );
+  }
+}
